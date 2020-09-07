bcf49b3634037cd1c07f864b7c9b15d1d21a60be
MIRAE <MIRAE@DESKTOP-6NH93BC>
2020-09-07 16:03:07 +0900

    test

---
+++ bugTest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+class LetDebug
+{
+public:
+    void printNum()
+    {
+        short s1 = 32767;
+        short s2 = 1;
+        int s3 = s1 + s2;
+        std::cout << s3 << std::endl;
+    }
+};
+
+int main()
+{
+    LetDebug* ld = new LetDebug;
+    ld->printNum();
+    return 0;
+}
